5f42103986f1910eec0029f8e51469b3c51f5a16
Nathan Gross <nathan.gross@gmail.com>
2025-05-18 18:37:19 -0500

    fix: added forgotten await

--- votes/loader/src/recursivelyProcessFiles.ts
+++ votes/loader/src/recursivelyProcessFiles.ts
@@ -7,7 +7,7 @@ import logger from './logger';
  * @param directory - The base directory to scan.
  * @param processingFunction - Function to run on each file.
  */
-export default async function recursivelyProcessFiles(directory: string, processingFunction: (file: string) => {}): Promise<void> {
+export default async function recursivelyProcessFiles(directory: string, processingFunction: (file: string) => Promise<void>): Promise<void> {
   try {
     const entries = await fs.readdir(directory, { withFileTypes: true });
     for (const entry of entries) {
@@ -19,7 +19,7 @@ export default async function recursivelyProcessFiles(directory: string, process
         logger.info({message: `Reading file: ${fullPath}`});
         try {
           const fileContent = await fs.readFile(fullPath, 'utf8');
-          processingFunction(fileContent);
+          await processingFunction(fileContent);
           logger.info({message: `Processed file: ${fullPath}`})
         } catch (err) {
           logger.error(`Error processing file: ${fullPath}`, err);
